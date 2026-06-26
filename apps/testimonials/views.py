a195755a6237d287d9eca348e21b350437170578
Aditya Mundada <47254711+AdityaMundada08@users.noreply.github.com>
2026-06-26 23:56:13 +0530

    Update testimonials views.py

--- apps/testimonials/views.py
+++ apps/testimonials/views.py
@@ -1,9 +1,11 @@
 from django.views.generic import ListView
 from .models import Testimonial
 
-
+# The embedded testimonials on home page
 class TestimonialListView(ListView):
     model = Testimonial
     template_name = 'testimonials/testimonial_list.html'
     context_object_name = 'testimonials'
     paginate_by = 12
+
+# Detailed view of testimonials on the testimonials page
