b61aa2d0180369cd14abc624ddc6339d6b1c4fc9
Aditya Mundada <47254711+AdityaMundada08@users.noreply.github.com>
2026-06-26 23:55:17 +0530

    Update treatement views.py

--- apps/treatments/views.py
+++ apps/treatments/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from .models import Treatment
 
-
+# For treatements list on home page
 class TreatmentListView(ListView):
     model = Treatment
     template_name = 'treatments/treatment_list.html'
     context_object_name = 'treatments'
     queryset = Treatment.objects.filter(is_active=True)
 
-
+# For detailed description on treatement, FAQs, etc
 class TreatmentDetailView(DetailView):
     model = Treatment
     template_name = 'treatments/treatment_detail.html'
